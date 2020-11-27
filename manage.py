3ea57529c638402918c9ce1bc792b1734afae87b
helenecourau <47565544+helenecourau@users.noreply.github.com>
2020-11-27 08:28:52 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "good_citizen.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "good_citizen.local_settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
